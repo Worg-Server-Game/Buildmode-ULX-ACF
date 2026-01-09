47bf7406a6c121af7e7c7671673d4d181b573d96
Duckmaster1654 <64399428+duckmaster1654@users.noreply.github.com>
2026-01-09 20:53:23 +0100

    updated because old version broke m9k

--- lua/autorun/explosivesfix.lua
+++ lua/autorun/explosivesfix.lua
@@ -1,11 +1,11 @@
 if not SERVER then return end
 
 hook.Add("EntityTakeDamage", "ACFExplosivesFix", function(target, dmg )
-    local inflictor = dmg:GetInflictor()
+    --local inflictor = dmg:GetInflictor()
 
-    if not target:IsPlayer() then return true end
-    if target:InVehicle() then return true end
-    if inflictor:IsPlayer() then return true end
+    --if not target:IsPlayer() then return true end
+    --if target:InVehicle() then return true end
+    --if inflictor:IsPlayer() or inflictor:GetOwner():IsPlayer() then return true end
 
-    return false
+    return dmg:IsExplosionDamage() and target:GetVehicle()
 end)
