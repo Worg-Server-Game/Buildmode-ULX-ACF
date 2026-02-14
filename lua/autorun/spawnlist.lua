ec90cfbec3029ec6759eee7b6837724c9101f4e0
Duckmaster1654 <64399428+duckmaster1654@users.noreply.github.com>
2026-02-14 12:52:31 +0100

    Update spawnlist.lua

--- lua/autorun/spawnlist.lua
+++ lua/autorun/spawnlist.lua
@@ -1,5 +1,4 @@
 local WeaponBlacklist = {
-    "m9k_minigun",
     "weapon_rpg",
 }
 
@@ -10,6 +9,12 @@ local EntityBlacklist = {
     "item_battery",
     "item_suitcharger",
     "prop_thumper",
+    "sent_ball",
+    "item_rpg_round",
+    "npc_grenade_frag",
+    "grenade_helicopter",
+    "acf_npc_base",
+    "sent_deployableballoons",
 }
 
 for _, value in pairs(WeaponBlacklist) do
